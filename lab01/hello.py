88888efc020ba1ea274e3f73eef3af95ee46dba8
Eric-Nangle <derickdangle222@gmail.com>
2026-09-17 20:44:12 +0000

    complete lab 1

---
+++ lab01/hello.py
@@ -0,0 +1,6 @@
+name = input("What is your name? ")
+course = input("What course are you studying? ")
+favourite = input("What are of cybersecurity interests you? ")
+print("Hello, " + name + "!")
+print("You are studying " + course)
+print("You are interested in " + favourite)
---
+++ lab01/student.py
@@ -0,0 +1,6 @@
+name = input("What is your name? ")
+student_id = input("What is your student number? ")
+year = input("What year are you currently in? ")
+print("Name: " +name)
+print("Student Number " +student_id)
+print("Year: " +year)
